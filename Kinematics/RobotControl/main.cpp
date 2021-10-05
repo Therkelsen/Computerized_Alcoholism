cf6aaa886db1081003e45153848e747f6a26c553
Therkelsen <thomastherkelsen191200@gmail.com>
2021-10-05 18:37:16 +0200

    Moved everything into a try catch

--- Kinematics/RobotControl/main.cpp
+++ Kinematics/RobotControl/main.cpp
@@ -10,34 +10,32 @@
 using namespace std;
 
 int main() {
+  // Create strings to contain ip adresses
   const std::string ipPhysical = "192.168.100.49";
   const std::string ipSimulator = "127.0.0.1";
   std::string ip = ipSimulator;
 
-
-  cout << "UR_RTDE: Attempting connection to robot socket at " << ip << " " << endl;
-  ur_rtde::RTDEControlInterface rtde_control(ip);
-
-  /*
   try {
+    // Connect to the robot socket
     cout << "UR_RTDE: Attempting connection to robot socket at " << ip << " " << endl;
     ur_rtde::RTDEControlInterface rtde_control(ip);
+
+    cout << "UR_RTDE: Connected to robot socket at " << ip << " " << endl;
+
+    // Send robot to base position
+    // (const std::vector<double> &q, double speed = 1.05, double acceleration = 1.4, bool async = false)
+    std::vector<double> jPose{-90,-90,-135,0,90,0};
+    double speed = 1;
+    double accel = 1;
+    bool async = false;
+    rtde_control.moveJ_IK(jPose, speed, accel, async);
 
   }
   catch (const runtime_error& error) {
     cout << "UR_RTDE: Failed connecting to robot socket at " << ip << " " << endl;
+    cout << "System : Exiting" << endl;
     return -1;
   }
-  */
-
-  cout << "UR_RTDE: Connected to robot socket at " << ip << " " << endl;
-
-  // (const std::vector<double> &q, double speed = 1.05, double acceleration = 1.4, bool async = false)
-  std::vector<double> jPose{-90,-90,-135,0,90,0};
-  double speed = 1;
-  double accel = 1;
-  bool async = false;
-  rtde_control.moveJ(jPose, speed, accel, async);
 
   return 0;
 }
