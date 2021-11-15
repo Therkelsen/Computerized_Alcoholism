e98f1d2b2cf43ff9c75a6eaf298a317970e362fb
Therkelsen <thomastherkelsen191200@gmail.com>
2021-11-15 14:58:02 +0100

    Renamed methods and names for more intuition, added methods for extracting intrinsics and distortion parameters from DB, added method for converting string to float array

---
+++ Database/build-db_test-Desktop-Debug/CMakeFiles/db_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/db_test.dir/database_func.cpp.o"
+  "CMakeFiles/db_test.dir/main.cpp.o"
+  "db_test"
+  "db_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/db_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
